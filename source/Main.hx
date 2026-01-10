b726b023c38a8962a395df42aea660dcb2242f16
JimmiesAndTheCoders <gageneest@gmail.com>
2026-01-10 17:01:25 -0400

    Get rid of those ugly jagged edges

--- source/Main.hx
+++ source/Main.hx
@@ -1,11 +1,13 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
 class Main extends Sprite {
 	public function new() {
 		super();
+		FlxSprite.defaultAntialiasing = true;
 		addChild(new FlxGame(0, 0, states.PlayState, 60, 60, true));
 	}
 }
